b65603ec40ebf13027d248b4fd54fa8edaa8e560
Franco Camus <francocamuspp@gmail.com>
2023-04-14 01:26:10 -0300

    Cambio estilo del nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,9 +10,8 @@ const Nav = function(){
                 <h3>Franco Camus</h3>
             </div>
             <div className="div-navLinks">
-                <Link to="Home" spy={true} smooth={true} offset={-100} duration={500}><h4 className="h4-about">INICIO</h4></Link>
                 <Link to="AboutMe" spy={true} smooth={true} offset={0} duration={500}><h4 className="h4-about">SOBRE MI</h4></Link>
-                <Link to="Resume" spy={true} smooth={true} offset={20} duration={500}><h4 className="h4-about">HABILIDADES</h4></Link>
+                <Link to="Resume" spy={true} smooth={true} offset={-50} duration={500}><h4 className="h4-about">HABILIDADES</h4></Link>
                 <Link to="Proyects" spy={true} smooth={true} offset={0} duration={500}><h4 className="h4-about">PROYECTOS</h4></Link>
                 <Link to="Contact" spy={true} smooth={true} offset={0} duration={500}><h4 className="h4-about">CONTACTO</h4></Link>
             </div>
